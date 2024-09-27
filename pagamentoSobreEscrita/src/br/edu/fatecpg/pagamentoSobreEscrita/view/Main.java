435b79fde50a673ac49baf2806e6ee6c337d200c
marcosharada24 <marcos.anna@fatec.sp.gov.br>
2024-09-27 15:34:08 -0300

    Add files via upload

--- pagamentoSobreEscrita/src/br/edu/fatecpg/pagamentoSobreEscrita/view/Main.java
+++ pagamentoSobreEscrita/src/br/edu/fatecpg/pagamentoSobreEscrita/view/Main.java
@@ -1,10 +1,17 @@
 package br.edu.fatecpg.pagamentoSobreEscrita.view;
 
+import br.edu.fatecpg.pagamentoSobreEscrita.model.Pagamento;
+import br.edu.fatecpg.pagamentoSobreEscrita.model.PagamentoBoleto;
+import br.edu.fatecpg.pagamentoSobreEscrita.model.PagamentoCartao;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	Pagamento pagamento1 = new PagamentoCartao();
+	Pagamento pagamento2 = new PagamentoBoleto();
+	
+	pagamento1.processarPagamento();
+	pagamento2.processarPagamento();
 	}
 
 }
